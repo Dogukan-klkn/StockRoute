c8dfc4d9273e0743b8a53066438fb2c8a21c0c35
Doğukan Kalkan <dogukankalkanfrt@gmail.com>
2026-07-16 11:39:45 +0300

    chore(shared-types): fix lint script and add eslint config

---
+++ packages/shared-types/.eslintrc.js
@@ -0,0 +1,5 @@
+// ESLint 8'in shareable-config isim çözümlemesi `@scope/eslint-config-*`
+// kalıbını beklediğinden, paketi mutlak dosya yoluyla (require.resolve) devralıyoruz.
+module.exports = {
+  extends: [require.resolve('@stockroute/config')],
+};
